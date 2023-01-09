c7f22847bd7daf5cd2189d696ee75a2e696268d3
devgony <devgony@gmail.com>
2023-01-09 22:49:16 +0900

    build: init spring boot project

---
+++ src/main/java/com/project/howsyourday/HowsYourDayApplication.java
@@ -0,0 +1,13 @@
+package com.project.howsyourday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowsYourDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowsYourDayApplication.class, args);
+	}
+
+}
